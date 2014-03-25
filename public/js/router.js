e36d3b06d2dbed783ea34284ab594134735ee457
Bobby Wilson and Christian Wesselhoeft <info+bobbywilson0+xtian@treelinedev.com>
2014-03-24 17:26:01 -0700

    Initialize model in setup controller hook.

--- public/js/router.js
+++ public/js/router.js
@@ -6,7 +6,8 @@ TypeFast.GameRoute = Ember.Route.extend({
   model: function() {
     return this.store.find('quote', Math.floor((Math.random()*3)+1));
   },
-  setupController: function(controller) {
+  setupController: function(controller, model) {
+    controller.set('model', model)
     controller.set('timer', TypeFast.Timer.create());
   }
 });
